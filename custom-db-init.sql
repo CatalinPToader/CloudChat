b6b017af9bf841b6ed4d0be2b12480bfccb248ac
Catalin Toader <petru.toader0807@stud.acs.upb.ro>
2024-01-12 00:15:33 +0200

    Added backend implementation (untested)

--- custom-db-init.sql
+++ custom-db-init.sql
@@ -1,25 +1,54 @@
-create table Tari (
- id serial primary key,
- nume_tara varchar(50),
- latitudine double precision,
- longitudine double precision
+create table listChannels
+(
+    id           uuid primary key,
+    channel_name varchar(255),
+    public       bool
 );
 
-create table Orase (
-id serial primary key,
-id_tara int,
-nume_oras varchar(50),
-latitudine double precision,
-longitudine double precision,
-constraint fk_tara foreign key(id_tara) references Tari(id) on delete cascade on update cascade);
+create table users
+(
+    id          uuid primary key,
+    username    varchar(255),
+    cookie      varchar(255),
+    online      bool,
+    lastChannel uuid,
+    constraint fk_channel foreign key (lastChannel) references listChannels (id) on update cascade
+);
 
-create table Temperaturi (
-id serial primary key,
-valoare double precision,
-timestamp timestamp DEFAULT CURRENT_TIMESTAMP,
-id_oras int,
-constraint fk_oras foreign key(id_oras) references orase(id) on delete cascade on update cascade);
+create table allowedChannel
+(
+    id        serial primary key,
+    channelID uuid,
+    userID    uuid,
+    constraint fk_user foreign key (userID) references users (id) on delete cascade on update cascade,
+    constraint fk_channel foreign key (channelID) references listChannels (id) on delete cascade on update cascade
+);
 
-alter table Tari add unique (nume_tara);
-alter table Orase add constraint OraseConstraint unique (id_tara, nume_oras);
-alter table Temperaturi add constraint TempConstraint unique (id_oras, timestamp);
+alter table users
+    add unique (username);
+
+alter table listChannels
+    add unique (channel_name);
+
+insert into listChannels (id, channel_name, public)
+VALUES (gen_random_uuid(), 'public_1', true);
+insert into listChannels (id, channel_name, public)
+VALUES (gen_random_uuid(), 'private_1', false);
+
+create table public_1
+(
+    id        serial primary key,
+    username  varchar(255),
+    timestamp timestamp DEFAULT CURRENT_TIMESTAMP,
+    message   text,
+    constraint fk_user foreign key (username) references users (id) on delete cascade on update cascade,
+);
+
+create table private_1
+(
+    id        serial primary key,
+    username  varchar(255),
+    timestamp timestamp DEFAULT CURRENT_TIMESTAMP,
+    message   text,
+    constraint fk_user foreign key (username) references users (id) on delete cascade on update cascade,
+);
