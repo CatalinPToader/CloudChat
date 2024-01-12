e5571d16927cfee1b6e989b271928d89e7149d31
Cristi <cristicristian1@gmail.com>
2024-01-12 15:39:18 +0200

    Added terraform configuration file for the backend deployment into the Kubernetes cluster. Added a Docker container that acts as local registry, similar to DockerHub, from which the Kubernetes nodes can pull the image of the backend

--- destroy-infra.sh
+++ destroy-infra.sh
@@ -3,6 +3,9 @@
 terraform destroy -auto-approve
 kind delete cluster --name goodcluster
 
+docker stop kind-registry
+docker rm kind-registry
+
 rm -rf .terraform
 rm .terraform.lock.hcl
 rm mykubeconfig
